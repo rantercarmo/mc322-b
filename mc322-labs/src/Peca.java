85d4cfd5477ac6b8ee9fbb3fd156aa62a0e355e9
Ranter Soares do Carmo <r186289@dac.unicamp.br>
2024-03-12 14:22:39 -0300

    adicionando a classe Peca

---
+++ mc322-labs/src/Peca.java
@@ -0,0 +1,31 @@
+public class Peca {
+
+    private String cor;
+    private int posicao;
+
+    //construtor
+    public Peca (String cor, int posicao){
+        this.cor = cor;
+        this.posicao = posicao;
+    }
+
+    //getters
+    public String getCor (){
+        return this.cor;
+    }
+
+    public int getPosicao(){
+        return this.posicao;
+    }
+
+    //setters
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+    public void setPosicao(int posicao){
+        this.posicao = posicao;
+    }
+
+}
